d85a93defffaccbd11ee83814f8b9a25e2e17fcd
corlard3y <corlardey@gmail.com>
2025-03-12 21:58:01 +0100

    update: index.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { getPreviewBasePath } from "../basePath";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
+const basename =
+  getPreviewBasePath() || import.meta.env.VITE_APP_PUBLIC_URL || "/";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter basename={basename}>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
-)
+);
