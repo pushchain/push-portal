10ee4b0f2d2d7274cd3d674f30edf7ef34c8afd3
corlard3y <corlardey@gmail.com>
2025-03-12 19:37:09 +0100

    fix: update package.json

---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
+import vitePluginRequire from "vite-plugin-require";
+import svgr from "vite-plugin-svgr";
+import topLevelAwait from "vite-plugin-top-level-await";
+import viteTsconfigPaths from "vite-tsconfig-paths";
+// https://vite.dev/config/
+export default defineConfig({
+    plugins: [
+        topLevelAwait(),
+        react(),
+        svgr(),
+        viteTsconfigPaths({
+            root: "./",
+        }),
+        nodePolyfills(),
+        vitePluginRequire.default(),
+    ],
+    define: {
+        global: "globalThis",
+    },
+    server: {
+        port: 3000,
+    },
+});
