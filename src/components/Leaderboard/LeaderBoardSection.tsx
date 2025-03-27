9bd137854b90930b7aa2d05de9eeee5b793bfe1d
corlard3y <corlardey@gmail.com>
2025-03-27 11:43:19 +0100

    fix: leaderboard tabs cache clear on tab switch

--- src/components/Leaderboard/LeaderBoardSection.tsx
+++ src/components/Leaderboard/LeaderBoardSection.tsx
@@ -1,17 +1,46 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { LeaderboardListS1 } from "./LeaderboardListS1";
 import { LeaderboardListS2 } from "./LeaderboardListS2";
 import { Box, Text } from "../../blocks";
+import { useQueryClient } from "@tanstack/react-query";
+import {
+  getRewardsLeaderboardS1,
+  getRewardsLeaderboardS2,
+  rewardsLeaderboardS1,
+  rewardsLeaderboardS2,
+} from "../../queries";
 
 export type LeaderBoardSectionProps = Record<string, never>;
 
 const LeaderBoardSection: FC<LeaderBoardSectionProps> = () => {
   const location = useLocation();
+  const queryClient = useQueryClient();
 
-  const season = location?.pathname?.includes("s1") ? "S1" : "S2";
+  const [season, setSeason] = useState(() =>
+    location.pathname.includes("s1") ? "S1" : "S2",
+  );
+
+  useEffect(() => {
+    const newSeason = location.pathname.includes("s1") ? "S1" : "S2";
+    setSeason(newSeason);
+
+    if (newSeason === "S1") {
+      queryClient.removeQueries({ queryKey: [rewardsLeaderboardS2] }); // Clear S2 cache
+      queryClient.prefetchInfiniteQuery({
+        queryKey: [rewardsLeaderboardS1],
+        queryFn: getRewardsLeaderboardS1,
+      });
+    } else {
+      queryClient.removeQueries({ queryKey: [rewardsLeaderboardS1] }); // Clear S1 cache
+      queryClient.prefetchInfiniteQuery({
+        queryKey: [rewardsLeaderboardS2],
+        queryFn: getRewardsLeaderboardS2,
+      });
+    }
+  }, [location.pathname, queryClient]);
 
   return (
     <Box
@@ -35,7 +64,7 @@ const LeaderBoardSection: FC<LeaderBoardSectionProps> = () => {
         Leaderboard {season}
       </Text>
 
-      {season === "S1" ? <LeaderboardListS1 /> : <LeaderboardListS2 />}
+      {season == "S1" ? <LeaderboardListS1 /> : <LeaderboardListS2 />}
     </Box>
   );
 };
