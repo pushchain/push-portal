58e3a2858616a0c33820f310a493032cd99366c9
corlard3y <corlardey@gmail.com>
2025-12-25 00:09:02 +0100

    chore: update rewards

--- src/App.tsx
+++ src/App.tsx
@@ -170,6 +170,7 @@ const AppContent = () => {
         >
           <Routes>
             <Route path="/" element={<Navigate to="/rewards/pre-launch" replace />} />
+            <Route path="/rewards" element={<Navigate to="/rewards/pre-launch" replace />} />
             {/*<Route path="/rewards" element={<RewardsPage />} />
             <Route path="/rewards/pushpass" element={<PushPassPage />} />*/}
             <Route path="/rewards/pre-launch" element={<PreLaunchPage />} />
